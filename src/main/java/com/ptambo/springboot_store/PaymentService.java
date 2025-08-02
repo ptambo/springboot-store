bfdc6da7059e8b672dc2a25b0ccd1b33163200fb
ptambo <philipp.tambornino@gmx.de>
2025-08-02 18:28:41 +0200

    finished tutorial

---
+++ src/main/java/com/ptambo/springboot_store/PaymentService.java
@@ -0,0 +1,5 @@
+package com.ptambo.springboot_store;
+
+public interface PaymentService {
+    void processPayment(double amount);
+}
--- src/main/java/com/ptambo/springboot_store/SpringbootStoreApplication.java
+++ src/main/java/com/ptambo/springboot_store/SpringbootStoreApplication.java
@@ -2,12 +2,15 @@ package com.ptambo.springboot_store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootStoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootStoreApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringbootStoreApplication.class, args);
+		var orderService = context.getBean(OrderService.class);
+		orderService.placeOrder();
 	}
 
 }
