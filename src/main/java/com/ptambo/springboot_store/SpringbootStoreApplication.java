6c170d824b06ea4ec170614533de99ee8949bce4
ptambo <philipp.tambornino@gmx.de>
2025-08-02 14:13:00 +0200

    init

---
+++ src/main/java/com/ptambo/springboot_store/SpringbootStoreApplication.java
@@ -0,0 +1,13 @@
+package com.ptambo.springboot_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ptambo/springboot_store/SpringbootStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptambo.springboot_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
